b33e7d59af1690ef89f36e4089647bcaf6331856
kashmafia <65639690+kashmafia@users.noreply.github.com>
2021-04-23 15:28:24 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn import metrics
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("healthcare-dataset-stroke-data.csv")
+
+X = data.drop(columns=['id', 'stroke', 'hypertension', 'heart_disease', 'ever_married', 'work_type', 'Residence_type', 'avg_glucose_level', 'bmi'])
+y = data['stroke']
+gender_dict = {'Male': 0, 'Female': 1, 'Other': 2}
+ever_married_dict = {'No': 0, 'Yes': 1}
+work_type_dict = {'children': 0, 'Never_worked': 1, 'Govt_job': 2, 'Private': 3, 'Self-employed': 4}
+residence_type_dict = {'Rural': 0, 'Urban': 1}
+smoking_status_dict = {'Unknown': 0, 'never smoked': 1, 'formerly smoked':2, 'smokes': 3}
+data['gender'] = data['gender'].map(gender_dict)
+data['ever_married'] = data['ever_married'].map(ever_married_dict)
+data['work_type'] = data['work_type'].map(work_type_dict)
+data['Residence_type'] = data['Residence_type'].map(residence_type_dict)
+data['smoking_status'] = data['smoking_status'].map(smoking_status_dict)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,random_state=109) # 70% training and 30% test
+
+#Create a svm Classifier
+clf = svm.SVC(kernel='linear') # Linear Kernel
+
+#Train the model using the training sets
+clf.fit(X_train, y_train)
+
+#Predict the response for test dataset
+y_pred = clf.predict(X_test)
+
+from sklearn import metrics
+
+# Model Accuracy: how often is the classifier correct?
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+print("Precision:",metrics.precision_score(y_test, y_pred))
+
+# Model Recall: what percentage of positive tuples are labelled as such?
+print("Recall:",metrics.recall_score(y_test, y_pred))
